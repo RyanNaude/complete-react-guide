52644958cff2fbcd323b547dc88f1a3f7a51630b
ryan-naude <ryan.naude@arwyp.com>
2022-04-05 12:25:21 +0200

    Started next module

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 /** Import Custom Components */
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+
 /** Import Custom Stylesheet */
 import "./ExpenseItem.css";
 
@@ -11,7 +12,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate expenses={props.expenses} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.expenses.title}</h2>
         <div className="expense-item__price">$ {props.expenses.amount}</div>
       </div>
     </Card>
